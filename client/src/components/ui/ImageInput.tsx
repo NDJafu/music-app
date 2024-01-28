c070a2ee73892a88cbb36007824c0f9b13969342
NDJafu <giaphu432@gmail.com>
2024-01-28 12:52:03 +0700

    feat(input): add ImageInput to be reusable component in forms

---
+++ client/src/components/ui/ImageInput.tsx
@@ -0,0 +1,21 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react"
+
+interface ImageInputProps extends React.ComponentPropsWithoutRef<"input"> {
+  openExplorerOnRender?: boolean
+}
+
+const ImageInput = forwardRef<HTMLInputElement, ImageInputProps>(
+  ({ openExplorerOnRender = false, ...props }, ref) => {
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    useImperativeHandle(ref, () => inputRef.current!, [])
+
+    useEffect(() => {
+      if (openExplorerOnRender) inputRef?.current?.click()
+    }, [])
+
+    return <input type="file" ref={inputRef} className="hidden" {...props} />
+  },
+)
+
+export default ImageInput
