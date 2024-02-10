48434ca7b076e343bc7948710fcfa3143732a9b3
NDJafu <phu.2174801030168@vanlanguni.vn>
2024-02-10 20:02:36 +0700

    refactor: rewrote entire profile dropdown

--- client/src/components/Profile/ProfileDropdown.tsx
+++ client/src/components/Profile/ProfileDropdown.tsx
@@ -1,91 +1,61 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { BsBoxArrowUpRight, BsChevronDown, BsPerson } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
-import Primary from '../Buttons/Primary';
+import { BsBoxArrowUpRight, BsPerson } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../app/hooks';
+import {
+  Dropdown,
+  DropdownContent,
+  DropdownItem,
+  DropdownSeparator,
+  DropdownTrigger,
+} from '../ui/Dropdown';
+import { useLogoutMutation } from '../../features/auth/authApiSlice';
 
-type Props = {
-  logOut: () => void;
-};
+const ProfileDropdown = () => {
+  const [logout, { isLoading }] = useLogoutMutation();
 
-const ProfileDropdown = ({ logOut }: Props) => {
-  const user = useAppSelector((state) => state.auth.currentUser);
-
-  const [showProfileMenu, setShowProfileMenu] = useState(false);
-  const profileRef: React.RefObject<HTMLDivElement> = useRef(null);
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      profileRef.current &&
-      !profileRef.current.contains(event.target as Node)
-    ) {
-      setShowProfileMenu(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
-    return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  }, []);
+  const navigate = useNavigate();
+  const currentUser = useAppSelector((state) => state.auth.currentUser);
 
   return (
-    <div ref={profileRef} className="relative h-8">
-      <Primary onClick={() => setShowProfileMenu(!showProfileMenu)}>
-        <div className="flex h-full items-center gap-1 p-1">
-          {!user?.avatar ? (
-            <div className="rounded-full bg-neutral-700 p-1">
-              <BsPerson size={16} />
-            </div>
-          ) : (
-            <img
-              src={user.avatar}
-              width={24}
-              height={24}
-              alt="avatar"
-              className="h-6 w-6 rounded-full object-cover"
-            />
-          )}
-          <BsChevronDown />
-        </div>
-      </Primary>
-      {showProfileMenu ? (
-        <div className="absolute right-0 top-10 flex w-44 flex-col rounded-md bg-neutral-800 p-1 text-sm">
-          <Link
-            to={'/account/overview'}
-            target="_blank"
-            className="flex items-center justify-between rounded-sm p-2.5 hover:bg-neutral-600"
-          >
-            <span>Account</span>
-            <BsBoxArrowUpRight />
-          </Link>
-          <Link
-            to={`/user/${user?.id}`}
-            className="rounded-sm p-2.5 hover:bg-neutral-600"
-          >
-            <span>Profile</span>
-          </Link>
-          <Link to={'/'} className="rounded-sm p-2.5 hover:bg-neutral-600">
-            <span>Notifications</span>
-          </Link>
-          <Link
-            to={'/'}
-            className="rounded-sm border-b border-neutral-700 p-2.5 hover:bg-neutral-600"
-          >
-            <span>Options</span>
-          </Link>
-          <button
-            onClick={logOut}
-            className="rounded-sm p-2.5 text-start hover:bg-neutral-600"
-          >
-            <span>Log out</span>
-          </button>
-        </div>
-      ) : (
-        ''
-      )}
-    </div>
+    <Dropdown>
+      <DropdownTrigger className="flex items-center rounded-full bg-jarcata p-1">
+        {currentUser?.avatar ? (
+          <img
+            src={currentUser?.avatar}
+            alt="avatar"
+            className="h-6 w-6 rounded-full object-cover"
+          />
+        ) : (
+          <div className="rounded-full bg-neutral-700 p-1">
+            <BsPerson size={16} />
+          </div>
+        )}
+      </DropdownTrigger>
+      <DropdownContent>
+        <DropdownItem
+          onClick={() =>
+            window.open(`${window.location.origin}/account/overview`)
+          }
+        >
+          Account
+          <BsBoxArrowUpRight />
+        </DropdownItem>
+        <DropdownItem onClick={() => navigate(`/user/${currentUser?.id}`)}>
+          Profile
+        </DropdownItem>
+        <DropdownItem onClick={() => {}}>Settings</DropdownItem>
+        <DropdownSeparator />
+        <DropdownItem
+          disabled={isLoading}
+          onClick={() => {
+            logout();
+            navigate('/');
+          }}
+        >
+          Log out
+        </DropdownItem>
+      </DropdownContent>
+    </Dropdown>
   );
 };
 
--- client/src/layout/MainLayout.tsx
+++ client/src/layout/MainLayout.tsx
@@ -1,53 +1,48 @@
-import React, { useState } from "react"
-import Sidebar from "../components/Sidebar"
-import { Link, Outlet } from "react-router-dom"
-import HistoryNavigation from "../components/Buttons/HistoryNavigation"
-import ProfileDropdown from "../components/Profile/ProfileDropdown"
-import { useAppSelector } from "../app/hooks"
-import PlayerBar from "../components/Player/PlayerBar"
-import Upload from "../components/Buttons/Upload"
-import { useLogoutMutation } from "../features/auth/authApiSlice"
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import { Link, Outlet } from 'react-router-dom';
+import HistoryNavigation from '../components/Buttons/HistoryNavigation';
+import ProfileDropdown from '../components/Profile/ProfileDropdown';
+import { useAppSelector } from '../app/hooks';
+import PlayerBar from '../components/Player/PlayerBar';
+import Upload from '../components/Buttons/Upload';
 
 const MainLayout = () => {
-  const [logout, { isLoading }] = useLogoutMutation()
-  const user = useAppSelector((state) => state.auth.currentUser)
+  const user = useAppSelector((state) => state.auth.currentUser);
 
-  const handleLogOut = async () => {
-    await logout()
-  }
-  const [isBlurred, setIsBlurred] = useState(false)
+  const [isBlurred, setIsBlurred] = useState(false);
 
   const handleBlur = (e: React.SyntheticEvent) => {
-    const target = e.target as HTMLTextAreaElement
-    setIsBlurred(target.scrollTop > 0)
-  }
+    const target = e.target as HTMLDivElement;
+    setIsBlurred(target.scrollTop > 0);
+  };
 
   return (
-    <div className="w-full h-screen flex flex-col bg-neutral-950">
+    <div className="flex h-screen w-full flex-col bg-neutral-950">
       <div className="flex flex-grow self-stretch overflow-hidden">
         <Sidebar />
         <div
-          className="w-full text-linkwater scroll-smooth overflow-auto"
+          className="w-full overflow-auto scroll-smooth text-linkwater"
           onScroll={handleBlur}
         >
           <div className="sticky top-0 z-10">
             <div
-              className={`absolute inset-x-0 px-9 py-6 inline-flex justify-between transition-all duration-500 ${
-                isBlurred && "bg-neutral-950/95 backdrop-blur"
+              className={`absolute inset-x-0 inline-flex justify-between px-9 py-6 transition-all duration-500 ${
+                isBlurred && 'bg-neutral-950/95 backdrop-blur'
               }`}
             >
               <HistoryNavigation />
               {!user ? (
                 <Link
-                  to={"account/login"}
-                  className="bg-jarcata rounded-full text-xs font-bold text-linkwater px-4 py-2"
+                  to={'account/login'}
+                  className="rounded-full bg-jarcata px-4 py-2 text-xs font-bold text-linkwater"
                 >
                   Đăng nhập
                 </Link>
               ) : (
                 <div className="flex gap-2">
                   <Upload />
-                  <ProfileDropdown logOut={handleLogOut} />
+                  <ProfileDropdown />
                 </div>
               )}
             </div>
@@ -57,7 +52,7 @@ const MainLayout = () => {
       </div>
       <PlayerBar />
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
