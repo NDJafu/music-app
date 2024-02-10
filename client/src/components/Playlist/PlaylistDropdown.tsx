c5fe2634da7b335e7c9c89b5a1ca0486cf0457a8
NDJafu <phu.2174801030168@vanlanguni.vn>
2024-02-10 19:53:21 +0700

    refactor: rewite sidebar create playlist dropdown

--- client/src/components/Playlist/PlaylistDropdown.tsx
+++ client/src/components/Playlist/PlaylistDropdown.tsx
@@ -1,67 +1,48 @@
-import React, { useEffect, useRef, useState } from 'react';
 import { BsCollection, BsPlus } from 'react-icons/bs';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { createNewPlaylist } from '../../features/playlist/playlistSlice';
+import { useAppSelector } from '../../app/hooks';
+import {
+  Dropdown,
+  DropdownContent,
+  DropdownItem,
+  DropdownTrigger,
+} from '../ui/Dropdown';
+import {
+  useCreatePlaylistMutation,
+  useGetPlaylistByUserQuery,
+} from '../../features/playlist/playlistApiSlice';
+import { useMemo } from 'react';
 
 const PlaylistDropdown = () => {
-  const [toggleModal, setToggleModal] = useState(false);
-  const dialogRef: React.RefObject<HTMLDivElement> = useRef(null);
-  const dispatch = useAppDispatch();
-  const currentUserPlaylists = useAppSelector(
-    (state) => state.playlist.currentUserPlaylist
-  );
+  const currentUser = useAppSelector((state) => state.auth.currentUser);
+  const { data: playlists } = useGetPlaylistByUserQuery(currentUser?.id!, {
+    skip: !currentUser,
+  });
+  const [createPlaylist] = useCreatePlaylistMutation();
 
-  const handleToggleModal = () => {
-    setToggleModal(!toggleModal);
+  const handleCreatePlaylist = () => {
+    const playlistsAmount = playlists?.filter(
+      (playlist) => playlist.userId.id == currentUser?.id
+    ).length;
+
+    createPlaylist({
+      title: `New Playlist #${playlistsAmount}`,
+    });
   };
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (
-      dialogRef.current &&
-      !dialogRef.current.contains(event.target as Node)
-    ) {
-      setToggleModal(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', handleClickOutside, true);
-    return () => {
-      document.removeEventListener('click', handleClickOutside, true);
-    };
-  }, []);
-
-  const handleCreateNewPlaylist = () => {
-    const numberOfPlaylists = currentUserPlaylists.length;
-    const defaultNewPlaylistTitle = `My Playlist #${numberOfPlaylists}`;
-
-    dispatch(createNewPlaylist(defaultNewPlaylistTitle));
-  };
   return (
-    <div className="relative -right-4 h-8 w-8">
-      <button
-        className="absolute inset-y-0 right-0"
-        onClick={handleToggleModal}
-      >
+    <Dropdown>
+      <DropdownTrigger className="flex items-center">
         <BsPlus size={32} />
-      </button>
-      {toggleModal && (
-        <>
-          <div
-            className="absolute left-0 top-10 w-48 rounded bg-neutral-800 p-1 text-base font-normal shadow-lg shadow-black/50"
-            ref={dialogRef}
-          >
-            <button
-              className="flex w-full items-center gap-2 rounded-sm p-2 hover:bg-white/5"
-              onClick={handleCreateNewPlaylist}
-            >
-              <BsCollection />
-              Create new playlist
-            </button>
+      </DropdownTrigger>
+      <DropdownContent rightSide>
+        <DropdownItem onClick={handleCreatePlaylist}>
+          <div className="flex items-center gap-2">
+            <BsCollection size={16} />
+            Create new playlist
           </div>
-        </>
-      )}
-    </div>
+        </DropdownItem>
+      </DropdownContent>
+    </Dropdown>
   );
 };
 
