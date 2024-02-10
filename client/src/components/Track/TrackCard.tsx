989650071bac43d836f6002d3f84b1c84b8d8fc9
NDJafu <phu.2174801030168@vanlanguni.vn>
2024-02-10 19:54:52 +0700

    style: responsive track card amount

--- client/src/components/Track/TrackCard.tsx
+++ client/src/components/Track/TrackCard.tsx
@@ -1,39 +1,47 @@
-import React, { useState } from "react"
-import { BsPlayFill } from "react-icons/bs"
-import { Track } from "../../app/types"
-import { useAppDispatch } from "../../app/hooks"
-import { addToQueue, playNewSong } from "../../features/player/playerSlice"
-import { useNavigate } from "react-router-dom"
+import React, { useState } from 'react';
+import { BsPlayFill } from 'react-icons/bs';
+import { Track } from '../../app/types';
+import { useAppDispatch } from '../../app/hooks';
+import { addToQueue, playNewSong } from '../../features/player/playerSlice';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
-  track: Track
-}
+  track: Track;
+};
 
 const TrackCard = ({ track }: Props) => {
-  const navigate = useNavigate()
-  const dispatch = useAppDispatch()
-  const [isOnPlayButton, setIsOnPlayButton] = useState(false)
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const [isOnPlayButton, setIsOnPlayButton] = useState(false);
   const handlePlaySong = () => {
-    dispatch(addToQueue(track))
-    dispatch(playNewSong())
-  }
+    dispatch(addToQueue(track));
+    dispatch(playNewSong());
+  };
   const handleViewSong = () => {
-    if (isOnPlayButton) return
-    navigate(`/track/${track.id}`)
-  }
+    if (isOnPlayButton) return;
+    navigate(`/track/${track.id}`);
+  };
   return (
     <div
-      className="bg-neutral-900 h-72 rounded-md p-4 overflow-hidden hover:bg-neutral-800 transition-colors duration-500 group cursor-pointer"
+      className="group hidden h-fit cursor-pointer overflow-hidden rounded-md bg-neutral-900 p-4 transition-colors
+       duration-500
+       hover:bg-neutral-800
+       2xl:block
+       sm:[&:nth-child(-n+2)]:block
+       md:[&:nth-child(-n+3)]:block
+       lg:[&:nth-child(-n+4)]:block
+       xl:[&:nth-child(-n+5)]:block
+       "
       onClick={handleViewSong}
     >
       <div className="relative">
         <img
           src={track.image}
           alt=""
-          className="w-full aspect-square object-cover rounded"
+          className="aspect-square w-full rounded object-cover"
         />
         <button
-          className="rounded-full w-12 aspect-square absolute -bottom-2 right-1 bg-jarcata-500 flex justify-center items-center opacity-0 group-hover:opacity-100 group-hover:bottom-1 transition-all duration-500"
+          className="absolute -bottom-2 right-1 flex aspect-square w-12 items-center justify-center rounded-full bg-jarcata-500 opacity-0 transition-all duration-500 group-hover:bottom-1 group-hover:opacity-100"
           onClick={handlePlaySong}
           onMouseOver={() => setIsOnPlayButton(true)}
           onMouseLeave={() => setIsOnPlayButton(false)}
@@ -41,10 +49,12 @@ const TrackCard = ({ track }: Props) => {
           <BsPlayFill size={28} />
         </button>
       </div>
-      <p className="mt-2 font-extrabold truncate">{track.title}</p>
-      <span className="opacity-60 text-sm">{track.artist}</span>
+      <div className="my-2">
+        <p className="truncate font-extrabold">{track.title}</p>
+        <p className="truncate text-sm opacity-60">{track.artist}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TrackCard
+export default TrackCard;
--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,27 +1,27 @@
-import { useAppSelector } from "../app/hooks"
-import { Track } from "../app/types"
-import TrackCard from "../components/Track/TrackCard"
-import TrackCardSkeleton from "../components/Skeletons/TrackCardSkeleton"
-import { useGetAllTrackQuery } from "../features/track/trackApiSlice"
+import { useAppSelector } from '../app/hooks';
+import { Track } from '../app/types';
+import TrackCard from '../components/Track/TrackCard';
+import TrackCardSkeleton from '../components/Skeletons/TrackCardSkeleton';
+import { useGetAllTrackQuery } from '../features/track/trackApiSlice';
 
 const Homepage = () => {
-  const currentUser = useAppSelector((state) => state.auth.currentUser)
-  const { data: tracks, isLoading } = useGetAllTrackQuery()
+  const currentUser = useAppSelector((state) => state.auth.currentUser);
+  const { data: tracks, isLoading } = useGetAllTrackQuery();
 
   const formattedTracks = tracks
     ?.filter((track) =>
-      currentUser?.id != track.uploader ? track.privacy == true : track,
+      currentUser?.id != track.uploader ? track.privacy == true : track
     )
     .reverse()
-    .slice(0, 7)
+    .slice(0, 7);
 
   return (
-    <div className="text-linkwater relative">
+    <div className="relative text-linkwater">
       {/* Actual content */}
       <div className="absolute h-88 w-full bg-gradient-to-b from-martinique to-neutral-950" />
-      <div className="top-20 px-9 relative h-[5000px]">
+      <div className="relative top-20 h-[5000px] px-9">
         <h2 className="text-3xl font-bold">New releases</h2>
-        <div className="grid 2xl:grid-cols-7 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 my-4 gap-2">
+        <div className="my-4 grid gap-2 overflow-hidden sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-7">
           {isLoading ? (
             <TrackCardSkeleton itemCount={7} />
           ) : (
@@ -32,7 +32,7 @@ const Homepage = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
